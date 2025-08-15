8bdee0707e3a8f2243593d61f0190decb9242d76
CalebeCampos <calebe_josue@hotmail.com>
2025-08-14 21:36:31 -0300

    arquivos criados e alterados ate o dia 20250814 as 2136

--- 05_automacaoo/commit_github.py
+++ 05_automacaoo/commit_github.py
@@ -8,7 +8,7 @@ def main():
     data_commit = input("Informe a data do commit: ").strip()
     hora_commit = input("Informe a hora do commit: ").strip()
 
-    gui.PAUSE = 0.5
+    gui.PAUSE = 0.8
 
     gui.hotkey("win","r") # abrir o executar
     gui.write('cmd') # digita o texto cmd
@@ -25,17 +25,16 @@ def main():
     gui.press("enter") # preciona a tecla enter
     gui.write('git add .')
     gui.press("enter") # preciona a tecla enter
-    gui.write(f'git commit -m "arquivos criados e alterados até o dia {data_commit} as {hora_commit}"')
+    gui.write(f'git commit -m "arquivos criados e alterados ate o dia {data_commit} as {hora_commit}"')
     gui.press("enter") # preciona a tecla enter
     gui.write('git push')
     gui.press("enter") # preciona a tecla enter
-
-
-#    gui.press("win") # preciona a tecla win
-#    gui.write("bloco de notas") # digita o texto bloco de notas
-#    time.sleep(1)
-#    gui.write("Ola mundo!") # digita o texto ola mundo
-
+    gui.write('git config --global --unset user.name')
+    gui.press("enter") # preciona a tecla enter
+    gui.write('git config --global --unset user.email')
+    gui.press("enter") # preciona a tecla enter
+    gui.write('exit')
+    gui.press("enter") # preciona a tecla enter
 
 if __name__ == "__main__":
     main()
