2e11860281c65e8c9913b2c20432bd109c91285e
CalebeCampos <calebe_josue@hotmail.com>
2025-08-15 19:28:29 -0300

    arquivos criados e alterados ate o dia 20250815 as 19:28

--- 05_automacao/commit_github.py
+++ 05_automacao/commit_github.py
@@ -2,12 +2,25 @@ from datetime import datetime
 import pyautogui as gui
 import time
 
+def remover_credenciais():
+    gui.write('git config --global --unset user.name')
+    gui.press("enter") # preciona a tecla enter
+    gui.write('git config --global --unset user.email')
+    gui.press("enter") # preciona a tecla enter
+
+def preencher_credenciais(usuario, email):
+    gui.write(f'git config --global user.name "{usuario}"')
+    gui.press("enter") # preciona a tecla enter
+    gui.write(f'git config --global user.email "{email}"')
+    gui.press("enter") # preciona a tecla enter
+
 def main():
 
     usuario = input("Informe seu usuario do gitHub: ").strip()
     email = input("Informe seu email do gitHub: ").strip()
     data_atual = datetime.now().strftime('%Y%m%d')
     hora_atual = datetime.now().strftime("%H:%M")
+    mensagem_commit = f'git commit -m "arquivos criados e alterados ate o dia {data_atual} as {hora_atual}"'
 
     gui.PAUSE = 0.8
 
@@ -16,26 +29,16 @@ def main():
     gui.press("enter") # preciona a tecla enter
     gui.write('cd C:\\Users\\ead\\PythonDev_CalebeCampos')
     gui.press("enter") # preciona a tecla enter
-    gui.write('git config --global --unset user.name')
-    gui.press("enter") # preciona a tecla enter
-    gui.write('git config --global --unset user.email')
-    gui.press("enter") # preciona a tecla enter
-    gui.write(f'git config --global user.name "{usuario}"')
-    gui.press("enter") # preciona a tecla enter
-    gui.write(f'git config --global user.email "{email}"')
-    gui.press("enter") # preciona a tecla enter
+    remover_credenciais()
+    preencher_credenciais(usuario, email)
     gui.write('git add .')
     gui.press("enter") # preciona a tecla enter
-    gui.write(f'git commit -m "arquivos criados e alterados ate o dia {data_atual} as {hora_atual}"')
+    gui.write(mensagem_commit)
     gui.press("enter") # preciona a tecla enter
     gui.write('git push')
     gui.press("enter") # preciona a tecla enter
-    gui.write('git config --global --unset user.name')
-    gui.press("enter") # preciona a tecla enter
-    gui.write('git config --global --unset user.email')
-    gui.press("enter") # preciona a tecla enter
-    #gui.write('exit')
-    #gui.press("enter") # preciona a tecla enter
+    remover_credenciais()
+
 
 if __name__ == "__main__":
     main()
