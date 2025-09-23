f27437153c14478fb850a2619fd65929df001a59
CalebeCampos <calebe_josue@hotmail.com>
2025-09-22 21:36:39 -0300

    arquivos criados e alterados ate o dia 20250922 as 2136

--- 11_desenvolvimento_web/02_django/projeto_crud/crud_app/urls.py
+++ 11_desenvolvimento_web/02_django/projeto_crud/crud_app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('cadastro/', views.cadastro_pessoa, name='cadastro_pessoa')
+    path('cadastrar/', views.cadastrar_pessoa, name='cadastrar_pessoa'),
+    path('alterar/<int:id_pessoa>/', views.alterar_pessoa, name='alterar_pessoa'),
+    path('excluir/<int:id_pessoa>/', views.excluir_pessoa, name='excluir_pessoa'),
+    path('pesquisar/', views.pesquisar_pessoa, name='pesquisar_pessoa')
 ]
--- 11_desenvolvimento_web/02_django/projeto_crud/crud_app/views.py
+++ 11_desenvolvimento_web/02_django/projeto_crud/crud_app/views.py
@@ -7,7 +7,7 @@ def home(request):
     pessoas = Pessoa.objects.all()
     return render(request, "home.html", {"pessoas":pessoas})
 
-def cadastro_pessoa(request):
+def cadastrar_pessoa(request):
     if request.method == 'POST':
         nome = request.POST.get('nome')
         email = request.POST.get('email')
@@ -15,3 +15,23 @@ def cadastro_pessoa(request):
         Pessoa.objects.create(nome=nome, email=email, cpf=cpf)
         return redirect('home')
     return render(request, 'cadastrar.html')
+
+def alterar_pessoa(request, id_pessoa):
+    pessoa = Pessoa.objects.get(id_pessoa=id_pessoa)
+    if request.method == 'POST':
+        pessoa.nome = request.POST.get('nome')
+        pessoa.email = request.POST.get('email')
+        pessoa.cpf = request.POST.get('cpf')
+        pessoa.save()
+        return redirect('home')
+    return render(request, "alterar.html", {"pessoa": pessoa})
+
+def excluir_pessoa(request, id_pessoa):
+    pessoa = Pessoa.objects.get(id_pessoa=id_pessoa)
+    pessoa.delete()
+    return redirect('home')
+
+def pesquisar_pessoa(request):
+    query = request.GET.get('input_pesquisar_pessoa', '')
+    pessoas = Pessoa.objects.filter(nome__icontains=query)
+    return render(request, 'home.html', {'pessoas':pessoas, 'query':query})
